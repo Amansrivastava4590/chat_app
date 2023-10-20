a534bd421248719a25ac783691e47a856355d285
Aman Srivastava <aman.srivastava@satat.tech>
2023-10-20 17:43:19 +0530

    group info

--- lib/pages/group_info.dart
+++ lib/pages/group_info.dart
@@ -17,6 +17,7 @@ class _GroupInfoState extends State<GroupInfo> {
   Stream? members;
   @override
   void initState() {
+    getMember();
     // TODO: implement initState
     super.initState();
   }
@@ -32,6 +33,63 @@ class _GroupInfoState extends State<GroupInfo> {
   String getName(String res){
     return res.substring(res.indexOf("_")+1);
   }
+  String getId(String res) {
+    return res.substring(0, res.indexOf("_"));
+  }
+
+  memberList() {
+    return StreamBuilder(
+      stream: members,
+      builder: (context, AsyncSnapshot snapshot) {
+        if (snapshot.hasData) {
+          if (snapshot.data['members'] != null) {
+            if (snapshot.data['members'].length != 0) {
+              return ListView.builder(
+                itemCount: snapshot.data['members'].length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+                    margin: EdgeInsets.symmetric(horizontal: 10),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Theme.of(context).primaryColor,
+                        child: Text(
+                          getName(snapshot.data['members'][index])
+                              .substring(0, 1)
+                              .toUpperCase(),
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      title: Text(getName(snapshot.data['members'][index])),
+                      subtitle: Text(getId(snapshot.data['members'][index])),
+                    ),
+                  );
+                },
+              );
+            } else {
+              return const Center(
+                child: Text("NO MEMBERS"),
+              );
+            }
+          } else {
+            return const Center(
+              child: Text("NO MEMBERS"),
+            );
+          }
+        } else {
+          return Center(
+              child: CircularProgressIndicator(
+                color: Theme.of(context).primaryColor,
+              ));
+        }
+      },
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,41 +102,48 @@ class _GroupInfoState extends State<GroupInfo> {
           IconButton(onPressed: (){}, icon: Icon(Icons.exit_to_app))
         ],
       ),
-      body: Container(
-        padding: EdgeInsets.all(20),
-        margin: EdgeInsets.symmetric(horizontal: 20,vertical: 20),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          color: Theme.of(context).primaryColor.withOpacity(0.2),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            CircleAvatar(
-              radius: 30,
-              backgroundColor: Theme.of(context).primaryColor,
-              child: Text(
-                  widget.groupName.substring(0,1).toUpperCase(),
-                style: TextStyle(fontWeight: FontWeight.w500,color: Colors.white),
-              ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.symmetric(horizontal: 20,vertical: 20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Theme.of(context).primaryColor.withOpacity(0.2),
             ),
-            SizedBox(width: 15,),
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Text("Group: ${widget.groupName}",style: TextStyle(
-                  fontWeight: FontWeight.w500
-                ),),
-                SizedBox(height: 5,),
-                
-                Text("Admin: ${getName(widget.adminName)}")
-              ],
-            )
+                CircleAvatar(
+                  radius: 30,
+                  backgroundColor: Theme.of(context).primaryColor,
+                  child: Text(
+                      widget.groupName.substring(0,1).toUpperCase(),
+                    style: TextStyle(fontWeight: FontWeight.w500,color: Colors.white),
+                  ),
+                ),
+                SizedBox(width: 15,),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Group: ${widget.groupName}",style: TextStyle(
+                      fontWeight: FontWeight.w500
+                    ),),
+                    SizedBox(height: 5,),
 
-          ],
-        ),
-      )
+                    Text("Admin: ${getName(widget.adminName)}")
+                  ],
+                )
+
+              ],
+            ),
+          ),
+
+          memberList()
+        ],
+      ),
+
     );
   }
 }
